682f34613867a83147435bcf33c81462c2e2ceed
Bryan White <bryanchriswhite@gmail.com>
2021-05-05 13:14:19 +0200

    add MsgSwapWithinBatch fuzz tests

---
+++ x/liquidity/types/tx_fuzz.pb.go
@@ -0,0 +1,73 @@
+// +build gofuzz
+
+package types
+
+import (
+	"bytes"
+	"errors"
+
+	gofuzz "github.com/google/gofuzz"
+	fleece "github.com/leastauthority/fleece/fuzzing"
+)
+
+func FuzzMsgSwapWithinBatch_raw(data []byte) int {
+	msg1 := MsgSwapWithinBatch{}
+	if err := msg1.Unmarshal(data); err != nil {
+		return fleece.FuzzNormal
+	}
+
+	msg1Data, err := msg1.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	msg2 := MsgSwapWithinBatch{}
+	if err := msg2.Unmarshal(msg1Data); err != nil {
+		panic(err)
+	}
+
+	msg2Data, err := msg1.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	if !bytes.Equal(msg1Data, msg2Data) {
+		panic(errors.New("serialized messages didn't match"))
+	}
+
+	return fleece.FuzzInteresting
+}
+
+func FuzzMsgSwapWithinBatch_structured(data []byte) int {
+	msg1 := MsgSwapWithinBatch{}
+
+	if len(data) == 0 {
+		return fleece.FuzzDiscard
+	}
+
+	f := gofuzz.NewFromGoFuzz(data)
+	f.NilChance(0)
+	f.NumElements(1, 100)
+	f.Fuzz(&msg1)
+
+	msg1Data, err := msg1.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	msg2 := MsgSwapWithinBatch{}
+	if err := msg2.Unmarshal(msg1Data); err != nil {
+		panic(err)
+	}
+
+	msg2Data, err := msg2.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	if bytes.Equal(msg1Data, msg2Data) {
+		panic(errors.New("serialized messages don't match"))
+	}
+
+	return fleece.FuzzNormal
+}
