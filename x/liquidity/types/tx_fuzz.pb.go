d3cd54628d6e85c9edec7de61d7440c2908baee8
Bryan White <bryanchriswhite@gmail.com>
2021-05-06 17:27:41 +0200

    add MsgWithdrawWithinBatch raw and structured

--- x/liquidity/types/tx_fuzz.pb.go
+++ x/liquidity/types/tx_fuzz.pb.go
@@ -75,3 +75,68 @@ func FuzzMsgSwapWithinBatch_structured(data []byte) int {
 
 	return fleece.FuzzNormal
 }
+func FuzzMsgWithdrawWithinBatch_raw(data []byte) int {
+	msg1 := MsgWithdrawWithinBatch{}
+	if err := msg1.Unmarshal(data); err != nil {
+		return fleece.FuzzNormal
+	}
+
+	msg1Data, err := msg1.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	msg2 := MsgWithdrawWithinBatch{}
+	if err := msg2.Unmarshal(msg1Data); err != nil {
+		panic(err)
+	}
+
+	if !reflect.DeepEqual(msg1, msg2) {
+		panic(errors.New("deserialized messages didn't' match"))
+	}
+
+	msg2Data, err := msg1.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	if !bytes.Equal(msg1Data, msg2Data) {
+		panic(errors.New("serialized messages didn't match"))
+	}
+
+	return fleece.FuzzInteresting
+}
+
+func FuzzMsgWithdrawWithinBatch_structured(data []byte) int {
+	msg1 := MsgWithdrawWithinBatch{}
+
+	if len(data) == 0 {
+		return fleece.FuzzDiscard
+	}
+
+	f := gofuzz.NewFromGoFuzz(data)
+	f.NilChance(0)
+	f.NumElements(1, 100)
+	f.Fuzz(&msg1)
+
+	msg1Data, err := msg1.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	msg2 := MsgWithdrawWithinBatch{}
+	if err := msg2.Unmarshal(msg1Data); err != nil {
+		panic(err)
+	}
+
+	msg2Data, err := msg2.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	if bytes.Equal(msg1Data, msg2Data) {
+		panic(errors.New("serialized messages don't match"))
+	}
+
+	return fleece.FuzzNormal
+}
