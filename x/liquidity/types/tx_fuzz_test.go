c4e6fec99267a3d1337086694c67f9f449dfbf8c
Bryan White <bryanchriswhite@gmail.com>
2021-05-09 10:33:41 +0200

    add triage tests for FuzzMsgWithdrawWithinBatch_{raw,structured}

--- x/liquidity/types/tx_fuzz_test.go
+++ x/liquidity/types/tx_fuzz_test.go
@@ -61,3 +61,19 @@ func TestFuzzMsgSwapWithinBatch_structured(t *testing.T) {
 
 	require.Zero(t, panics)
 }
+
+func TestFuzzMsgWithdrawWithinBatch_raw(t *testing.T) {
+	_, panics, _ := fleece.
+		MustNewCrasherIterator(env, FuzzMsgWithdrawWithinBatch_raw, filters...).
+		TestFailingLimit(t, crashLimit)
+
+	require.Zero(t, panics)
+}
+
+func TestFuzzMsgWithdrawWithinBatch_structured(t *testing.T) {
+	_, panics, _ := fleece.
+		MustNewCrasherIterator(env, FuzzMsgWithdrawWithinBatch_structured, filters...).
+		TestFailingLimit(t, crashLimit)
+
+	require.Zero(t, panics)
+}
